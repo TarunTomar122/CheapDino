713dea7e47f69a03a69eeda4e76e0d83db809e37
TarunTomar122 <tomartarun2001@gmail.com>
2020-03-20 17:14:16 +0530

    Game Completed

--- main.py
+++ main.py
@@ -3,20 +3,31 @@ import pygame
 WIDTH = 600
 HEIGHT = 400
 FPS = 30
-TOTALPLAYERS = 10
+TOTALPLAYERS = 1
 FPSCLOCK = 30
+MAXJUMP = 18
+
+playerX = 50
 
 IMAGES = {}
 
+
+def checkCollision(playerY, cactus):
+    status = []
+    for player in range(TOTALPLAYERS):
+        status.append(False)
+        for cac in range(len(cactus)):
+            if cactus[cac]['x'] <= (playerX + 48) and playerY[player] >= HEIGHT - 128:
+                status[player] = True
+    return status
+
+
 def main():
-    global FPSCLOCK,SCREEN,IMAGES
+    global FPSCLOCK, SCREEN, IMAGES
 
     pygame.init()
     pygame.display.set_caption('Dino Hack')
-
-
-
-    SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+    SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
     FPSCLOCK = pygame.time.Clock()
 
     IMAGES['cactus'] = pygame.image.load('./cactus.png')
@@ -24,55 +35,86 @@ def main():
 
     while True:
         playGame()
+        print("New Generation")
+
 
 def playGame():
-    global FPSCLOCK,SCREEN
+    global FPSCLOCK, SCREEN, TOTALPLAYERS
+
+    alivePlayers = TOTALPLAYERS
 
     running = True
 
     cacShift = -4
 
-    playerVel = []
+    playerYlist = []
+
+    playerJump = []
+    for player in range(TOTALPLAYERS):
+        playerJump.append(-1-MAXJUMP)
+
     playerDownAcc = 4
-    playerUpVel = -5
+    playerPress = -200
+    playerState = []
+
+    playerMinY = 200
 
     for player in range(TOTALPLAYERS):
-        playerVel.append(HEIGHT-IMAGES['player'].get_height())
+        playerYlist.append(HEIGHT-IMAGES['player'].get_height())
+        playerState.append(True)
 
     cactus = [
-        {'x':WIDTH//2, 'y':HEIGHT-IMAGES['cactus'].get_height()},
-        {'x':WIDTH, 'y':HEIGHT-IMAGES['cactus'].get_height()}
+        {'x': WIDTH//2, 'y': HEIGHT-IMAGES['cactus'].get_height()},
+        {'x': WIDTH, 'y': HEIGHT-IMAGES['cactus'].get_height()}
     ]
 
     while running:
 
-        SCREEN.fill((255,255,255))
+        SCREEN.fill((255, 255, 255))
+
+        status = checkCollision(playerYlist, cactus)
+
+        for player in range(TOTALPLAYERS):
+            if status[player]:
+                playerState[player] = False
+                alivePlayers -= 1
+
+        if alivePlayers == 0:
+            running = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 for player in range(TOTALPLAYERS):
-                    playerVel[player] = playerUpVel
+                    # if playerState[player] and playerYlist[player]>playerMinY:
+                    #     playerYlist[player] += playerPress
+                    if playerJump[player] == (-MAXJUMP-1):
+                        playerJump[player] = MAXJUMP
 
         for player in range(TOTALPLAYERS):
-            if playerVel[player]>HEIGHT-IMAGES['player'].get_height():
-                playerVel[player] += playerDownAcc
+            if playerState[player]:
+                if playerJump[player] >= -MAXJUMP:
+                    playerYlist[player] -= playerJump[player]
+                    playerJump[player] -= 1
 
         for player in range(TOTALPLAYERS):
-            SCREEN.blit(IMAGES['player'],(0,playerVel[player]))
+            if playerState[player]:
+                SCREEN.blit(IMAGES['player'], (playerX, playerYlist[player]))
 
         for cac in cactus:
-            cac['x'] += cacShift 
+            cac['x'] += cacShift
 
-        if cactus[0]['x']<=0:
-            cactus.append({'x':WIDTH, 'y':HEIGHT-IMAGES['cactus'].get_height()})
+        if cactus[0]['x'] <= 0:
+            cactus.append({'x': WIDTH, 'y': HEIGHT -
+                           IMAGES['cactus'].get_height()})
             cactus.pop(0)
 
         for cac in cactus:
-            SCREEN.blit(IMAGES['cactus'],(cac['x'],cac['y']))   
+            SCREEN.blit(IMAGES['cactus'], (cac['x'], cac['y']))
 
         pygame.display.update()
-        FPSCLOCK.tick(FPS)   
+        FPSCLOCK.tick(FPS)
 
-main()        
+
+main()
